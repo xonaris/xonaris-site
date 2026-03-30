bf8a46de59abc755fb8e3d9298749d8de82e83f2
Fabien Arcouet <FabienArcouet@proton.me>
2026-03-30 23:36:15 +0200

    Delete src directory

--- src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const CurrentUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    const user = request.user;
-    return data ? user?.[data] : user;
-  },
-);
